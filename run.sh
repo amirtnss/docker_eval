660a02751b68a83f939ce2b158f2101b0633a564
sidia <amir.sidia@etu-sorbonne.universite.fr>
2023-01-27 14:00:18 +0100

    ex1

---
+++ run.sh
@@ -0,0 +1,12 @@
+#ex2
+
+docker network create mynetwork
+
+
+docker run --network=mynetwork --name worker -d eval/worker
+
+
+docker run --network=mynetwork --name worker1 -e PORT=8081 -d eval/worker
+
+
+docker run --network=mynetwork --name planner -d eval/planner
---
+++ stop.sh
@@ -0,0 +1,9 @@
+#ex2
+
+docker stop worker planner
+
+
+docker rm worker planner
+
+
+docker network rm mynetwork
